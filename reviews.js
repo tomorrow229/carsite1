9226153539e9c14825806b8754be5cf109fdb9a9
tomorrow229 <ep220247@gmail.com>
2025-06-02 18:39:31 +0500

    Create reviews.js

---
+++ reviews.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, { threshold: 0.3 });
+
+  cards.forEach(card => observer.observe(card));
+});
